448a1c1405a1aa556c0ab8d036b5586f3805215b
Paweł Kołodziejczak <modwebpro@gmail.com>
2018-11-22 00:48:11 +0100

    Update aos.js
    + delay timeout
    + calculation of scroll by bottom edge of the screen

--- aos.js
+++ aos.js
@@ -1,24 +1,30 @@
 export class aos {
-  constructor() {
-    this.init();
+  constructor(className = '.aos') {
+    this.init(className);
   }
 
-  init() {
-    if (!this._setVars()) return;
+  init(className) {
+    if (!this._setVars(className)) return;
     this._setEvents();
   }
 
-  _setVars() {
-    let _this = this;
+  _setVars(className) {
+    var _this = this;
 
-    _this._aos = document.querySelectorAll('.aos');
+    _this._body = document.getElementsByTagName('body')[0];
+    if (!_this._body) return false;
+
+    _this._aos = _this._body.querySelectorAll(className);
     if (!_this._aos.length) return false;
 
     _this._scroll = {};
     _this._data = {};
+    _this._timer = {};
     _this._arr = [];
 
     EasePack;
+    CSSPlugin;
+
     return true;
   }
 
@@ -29,25 +35,27 @@ export class aos {
 
   _setOffset() {
     each(this._aos, (key, val) => {
-      let offset = val.getAttribute('data-offset') || 0;
+      let offset = parseInt(val.getAttribute('data-offset')) || 0;
 
       this._data['act' + key] = false;
-      this._data['rel' + key] = parseInt(getOffset(val).t - offset);
+      this._data['rel' + key] = getOffset(val).t + offset;
+      this._data['delay' + key] = val.getAttribute('data-delay') || 0;
 
       TweenLite.set(val, {autoAlpha: 0, y: '50%'})
     });
   }
 
   _onScroll() {
-    let _this = this;
+    var _this = this;
 
     this._scroll._fn = this._scroll._fn || {};
     this._scroll._fn.aos = () => {
-      let sTop = document.body.scrollTop || document.documentElement.scrollTop;
+      let sTop = document.body.scrollTop || document.documentElement.scrollTop,
+        relTop = sTop + window.innerHeight;
 
       for (let key = 0; key <= this._aos.length; key++) {
 
-        if (sTop >= this._data['rel' + key] && !this._data['act' + key]) {
+        if (relTop >= this._data['rel' + key] && !this._data['act' + key]) {
 
           this._data['act' + key] = true;
 
@@ -58,8 +66,12 @@ export class aos {
               _this._update(key);
             }
           });
-          tl.play().to(this._aos[key], 1.5, {y: '0%', ease: Expo.easeOut})
-            .to(this._aos[key], 1.5, {autoAlpha: 1}, '-=1.2');
+
+
+          this._timer = setTimeout(() => {
+            tl.play().to(this._aos[key], 1.5, {y: '0%', ease: Expo.easeOut})
+              .to(this._aos[key], 1.5, {autoAlpha: 1}, '-=1.2');
+          }, parseFloat(this._data['delay' + key]) * 1000);
         }
       }
     };
@@ -75,11 +87,13 @@ export class aos {
     try {
       this._scroll._fn.aos ? document.removeEventListener('scroll', this._scroll._fn.aos) : '';
 
+      clearTimeout(this._timer);
       Object.entries(this).forEach(([key, val]) => {
         delete this[key];
       });
+
     } catch (e) {
-      console.error(e.message);
+      console.error(e);
     }
   }
 }
